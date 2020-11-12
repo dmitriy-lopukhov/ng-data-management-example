d3b0eb663c0835e1c7258c288f6b94e878dfcc25
Dmitriy Lopukhov <dmitriy@admins-MacBook-Pro.local>
2020-11-13 00:27:06 +0100

    added some animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/movie/movies/movies.component.ts
+++ src/app/movie/movies/movies.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
 import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 import { IMovie } from "@app/core/services/entities/movie/movie.model";
 import { MovieService } from "@app/core/services/entities/movie/movie.service";
@@ -8,6 +15,18 @@ import { Observable } from "rxjs";
   templateUrl: "./movies.component.html",
   styleUrls: ["./movies.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger("movieCardAnimate", [
+      state("in", style({ transform: "translateY(0)", opacity: 1 })),
+      transition("void => *", [
+        style({ transform: "translateY(10%)", opacity: 0 }),
+        animate(500),
+      ]),
+      transition("* => void", [
+        animate(300, style({ transform: "translateY(10%)", opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class MoviesComponent implements OnInit {
   movies$: Observable<IMovie[]>;
