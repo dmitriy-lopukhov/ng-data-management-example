916d45439208de080656bdd693ee16e64d60d80e
Dmitriy Lopukhov <dmitriy@admins-MacBook-Pro.local>
2020-11-14 01:03:01 +0100

    ng update

--- src/app/movie/movies/movies.component.spec.ts
+++ src/app/movie/movies/movies.component.spec.ts
@@ -1,9 +1,4 @@
-import {
-  async,
-  ComponentFixture,
-  TestBed,
-  waitForAsync,
-} from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 
 import { MoviesComponent } from "./movies.component";
 
